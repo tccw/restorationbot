1b07a55759db35565ef4e44be376b4f7cfce7ad1
tccw <thomas.cowan@gmail.com>
2020-10-25 21:44:01 -0700

    GcloudInstance and Bucket moved to the same file.

--- gcloud_resources.py
+++ gcloud_resources.py
@@ -1,10 +1,13 @@
+import time
 import pickle
-from CustomExceptions import *
-from pathlib import Path
+from config import ZONE, PROJECT_NAME, VM_NAME
+from googleapiclient import discovery
 from google.cloud import storage
+from pathlib import Path
 
 from common import delete_dir_contents
 from config import FILETYPE_SET
+from CustomExceptions import *
 
 
 class Bucket:
@@ -77,3 +80,61 @@ class Bucket:
 
     def list_bucket_objects(self) -> [str]:
         return list(self._storage_client.list_blobs(self.bucket_name))
+
+
+class ComputeInstance:
+
+    def __init__(self, service_name: str, version: str):
+        self.compute = discovery.build(service_name, version)  # Google API resource object
+        self.operation = None
+        self.project = None
+        self.zone = None
+        self.name = None
+
+    def get_existing_instance(self, project=PROJECT_NAME, zone=ZONE, name=VM_NAME):
+        self.project = project
+        self.zone = zone
+        self.name = name
+        self.operation = self.compute.instances().get(
+            project=self.project,
+            zone=self.zone,
+            instance=self.name).execute()
+
+    def status(self):
+        if self.operation is None:
+            return "Object has no current operation/instance."
+        return self.operation['status']
+
+    def _wait_for_operation(self) -> object:
+
+        while self.operation is not None:
+            result = self.compute.zoneOperations().get(
+                project=self.project,
+                zone=self.zone,
+                operation=self.operation).execute()
+
+            if result['status'] == 'DONE':
+                if 'error' in result:
+                    raise Exception(result['error'])
+                return result
+
+            time.sleep(1)
+
+    def delete_instance(self) -> None:
+        return self.compute.instances().delete(
+            project=self.project,
+            zone=self.zone,
+            instance=self.name).execute()
+
+    def stop_instance(self):
+        return self.compute.instances().stop(
+            project=self.project,
+            zone=self.zone,
+            instance=self.name).execute()
+
+    def start_instance(self):
+        if self.operation is not None and self.status() != 'RUNNING':
+            self.compute.instances().start(
+                project=self.project,
+                zone=self.zone,
+                instance=self.name).execute()
--- run.py
+++ run.py
@@ -1,40 +1,31 @@
-from GcloudInstance import GcloudInstance
-from RedditBot import RedditBot
-from Bucket import Bucket
-from config import BUCKET, CREDENTIALS, IMGUR_CONFIG
 import os
-from imgur_python import Imgur
 
-# TODO run scratch healing only if scratch detected within face bounding box
+from gcloud_resources import ComputeInstance
+from gcloud_resources import Bucket
+from RedditBot import RedditBot
+from config import BUCKET, CREDENTIALS
 
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = CREDENTIALS
 
 # initialize working objects
-imgur_client = Imgur(IMGUR_CONFIG)
-reddit = RedditBot(sub='testingground4bots')
+reddit = RedditBot()
 bucket = Bucket(BUCKET)
-gcloud = GcloudInstance('compute', 'v1')
+gcloud = ComputeInstance('compute', 'v1')
 gcloud.get_existing_instance()
-
-
 
 # find posts and upload their images to the gcloud bucket
 reddit.monitor_posts()
-
 reddit.dump_images('raw_images')
 bucket.upload_dir('raw_images')
-reddit.print_titles()
 print('waiting...')
 
-
 # start up the VM. This will run the startup script, copying and processing all the images
-#TODO scratch detection and run restoration with scratches
-gcloud.start_instance()
+# gcloud.start_instance()
 print('waiting for status change')
-bucket.download_dir('processed_images/')
 # stop the VM to save money and run the shutdown script to copy processed files to the bucket
-gcloud.stop_instance()
+# gcloud.stop_instance()
 # download the processed images to the local machine with verification
-reddit.reply_all_subs(imgur_client)
+bucket.download_dir('processed_images/')
+
 # clean up the raw_images folder on the bucket and
 bucket.clean_up_bucket()
